9d6cad37a777f9b466358be56acca8d3ae199d6d
steve.brazier <meadsteve@gmail.com>
2015-11-08 20:48:24 +0000

    first draft of string even odd

--- lib/elixir_golf.ex
+++ lib/elixir_golf.ex
@@ -1,2 +1,16 @@
 defmodule ElixirGolf do
+  def start do
+    import String
+    import Enum
+    :stdio
+      |> IO.gets
+      |> strip
+      |> codepoints
+      |> map(&to_integer/1)
+      |> map(fn
+        x when rem(x , 2) == 0  -> "e"
+        x                       -> "o"
+      end)
+      |> join
+  end
 end
