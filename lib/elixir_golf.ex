1ac913a8425967cab4eae49fcad90d308d5ad174
steve.brazier <meadsteve@gmail.com>
2015-11-10 20:06:04 +0000

    remove prompt for shortness

--- lib/elixir_golf.ex
+++ lib/elixir_golf.ex
@@ -1,5 +1,5 @@
 defmodule ElixirGolf do
   def start do
-    import String;for s<-:stdio|>IO.gets|>codepoints,s !="\n",x=s|>to_integer|>rem(2),do: Enum.at'eo',x
+    import String;for s<-""|>IO.gets|>codepoints,s !="\n",x=s|>to_integer|>rem(2),do: Enum.at'eo',x
   end
 end
